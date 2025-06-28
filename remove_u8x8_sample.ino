090faf4cc8931b0746a077d781e02bb0f7a0d348
Keita Nakata <0024481294bxyk6037@gmail.com>
2025-06-28 13:03:23 +0900

    Create remove_u8x8_sample.ino

---
+++ remove_u8x8_sample.ino
@@ -0,0 +1,210 @@
+#include <Arduino.h>
+
+// #define NODE_SLAVE
+
+static char recv_buf[512];
+static bool is_exist = false;
+
+static int at_send_check_response(char *p_ack, int timeout_ms, char*p_cmd, ...)
+{
+    int ch = 0;
+    int index = 0;
+    int startMillis = 0;
+    va_list args;
+    memset(recv_buf, 0, sizeof(recv_buf));
+    va_start(args, p_cmd);
+    Serial1.printf(p_cmd, args);
+    Serial.printf(p_cmd, args);
+    va_end(args);
+    delay(200);
+    startMillis = millis();
+
+    if (p_ack == NULL)
+    {
+        return 0;
+    }
+
+    do
+    {
+        while (Serial1.available() > 0)
+        {
+            ch = Serial1.read();
+            recv_buf[index++] = ch;
+            Serial.print((char)ch);
+            delay(2);
+        }
+
+        if (strstr(recv_buf, p_ack) != NULL)
+        {
+            return 1;
+        }
+
+    } while (millis() - startMillis < timeout_ms);
+    return 0;
+}
+
+static int recv_prase(void)
+{
+    char ch;
+    int index = 0;
+    memset(recv_buf, 0, sizeof(recv_buf));
+    while (Serial1.available() > 0)
+    {
+        ch = Serial1.read();
+        recv_buf[index++] = ch;
+        Serial.print((char)ch);
+        delay(2);
+    }
+
+    if (index)
+    {
+        char *p_start = NULL;
+        char data[32] = {
+            0,
+        };
+        int rssi = 0;
+        int snr = 0;
+
+        p_start = strstr(recv_buf, "+TEST: RX \"5345454544");
+        if (p_start)
+        {
+            p_start = strstr(recv_buf, "5345454544");
+            if (p_start && (1 == sscanf(p_start, "5345454544%s", data)))
+            {
+                data[4] = 0;
+                Serial.print("RX: 0x");
+                Serial.print(data);
+                Serial.print("\r\n");
+            }
+
+            p_start = strstr(recv_buf, "RSSI:");
+            if (p_start && (1 == sscanf(p_start, "RSSI:%d,", &rssi)))
+            {
+                Serial.print("rssi:");
+                Serial.print(rssi);
+                Serial.print("\r\n");
+            }
+            p_start = strstr(recv_buf, "SNR:");
+            if (p_start && (1 == sscanf(p_start, "SNR:%d", &snr)))
+            {
+                Serial.print("snr :");
+                Serial.print(snr);
+                Serial.print("\r\n");
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
+static int node_recv(uint32_t timeout_ms)
+{
+    at_send_check_response("+TEST: RXLRPKT", 1000, "AT+TEST=RXLRPKT\r\n");
+    int startMillis = millis();
+    do
+    {
+        if (recv_prase())
+        {
+            return 1;
+        }
+    } while (millis() - startMillis < timeout_ms);
+    return 0;
+}
+
+static int node_send(void)
+{
+    static uint16_t count = 0;
+    int ret = 0;
+    char data[32];
+    char cmd[128];
+
+    memset(data, 0, sizeof(data));
+    sprintf(data, "%04X", count);
+    sprintf(cmd, "AT+TEST=TXLRPKT,\"5345454544%s\"\r\n", data);
+
+    Serial.print("TX: 0x");
+    Serial.print(data);
+    Serial.print("\r\n");
+
+    ret = at_send_check_response("TX DONE", 2000, cmd);
+    if (ret == 1)
+    {
+        count++;
+        Serial.print("送信成功！\r\n");
+    }
+    else
+    {
+        Serial.print("送信失敗！\r\n");
+    }
+    return ret;
+}
+
+static void node_recv_then_send(uint32_t timeout)
+{
+    int ret = 0;
+    ret = node_recv(timeout);
+    delay(100);
+    if (!ret)
+    {
+        Serial.print("\r\n");
+        return;
+    }
+    node_send();
+    Serial.print("\r\n");
+}
+
+static void node_send_then_recv(uint32_t timeout)
+{
+    int ret = 0;
+    ret = node_send();
+    if (!ret)
+    {
+        Serial.print("\r\n");
+        return;
+    }
+    if (!node_recv(timeout))
+    {
+        Serial.print("受信タイムアウト！\r\n");
+    }
+    Serial.print("\r\n");
+}
+
+void setup(void)
+{
+    Serial.begin(115200);
+    // while (!Serial);
+
+    Serial1.begin(9600);
+    Serial.print("ピンポン通信！\r\n");
+
+    if (at_send_check_response("+AT: OK", 100, "AT\r\n"))
+    {
+        is_exist = true;
+        at_send_check_response("+MODE: TEST", 1000, "AT+MODE=TEST\r\n");
+        at_send_check_response("+TEST: RFCFG", 1000, "AT+TEST=RFCFG,866,SF12,125,12,15,14,ON,OFF,OFF\r\n");
+        delay(200);
+#ifdef NODE_SLAVE
+        Serial.print("スレーブモード\r\n");
+#else
+        Serial.print("マスターモード\r\n");
+#endif
+    }
+    else
+    {
+        is_exist = false;
+        Serial.print("E5モジュールが見つかりません。\r\n");
+    }
+}
+
+void loop(void)
+{
+    if (is_exist)
+    {
+#ifdef NODE_SLAVE
+        node_recv_then_send(2000);
+#else
+        node_send_then_recv(2000);
+        delay(3000);
+#endif
+    }
+}
